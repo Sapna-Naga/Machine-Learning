97a42acc326d0e8a049fd5aeb848e06e167f1b0e
Sapna Naga <77587791+Sapna-Naga@users.noreply.github.com>
2021-01-28 20:07:51 +0530

    Regressor

--- XGBoost/Regressor/XGBoost.py
+++ XGBoost/Regressor/XGBoost.py
@@ -15,19 +15,19 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Training XGBoost on the Training set
-from xgboost import XGBClassifier
-classifier = XGBClassifier()
-classifier.fit(X_train, y_train)
+from xgboost import XGBRegressor
+regressor = XGBRegressor()
+regressor.fit(X_train, y_train)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
+y_pred = regressor.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
 print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
 print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
